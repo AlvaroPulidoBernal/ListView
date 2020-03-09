5776aa0a7af9738891f04ae60eb33ea07f61e692
Alvaro_Pulido <alvaro_pulidobernal@outlook.com>
2020-03-09 13:55:36 -0600

    Proyect files

---
+++ app/src/main/java/com/apulido/listview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.apulido.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TextView textView;
+    public ListView listView;
+
+    public String nombres[] = {"Pepe","Samuel","Valentina","José","Andrea","Karen","Armando"};
+    public String edades[] = {"23","33","19","45","32","12","23"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView1);
+        listView = findViewById(R.id.listview1);
+
+        ArrayAdapter <String> array_adapter = new ArrayAdapter<String>(this, R.layout.list_item_listview, nombres);
+        listView.setAdapter(array_adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               textView.setText("Edad de " + listView.getItemAtPosition(position) + " es de " + edades[position] + " años");
+            }
+        });
+    }
+}
