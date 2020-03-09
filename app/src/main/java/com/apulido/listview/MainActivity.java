475b17940b055fc8ee8026493c55995f1ec1efed
Alvaro_Pulido <alvaro_pulidobernal@outlook.com>
2020-03-09 17:32:29 -0600

    Comment changes

--- app/src/main/java/com/apulido/listview/MainActivity.java
+++ app/src/main/java/com/apulido/listview/MainActivity.java
@@ -23,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     public String edades[] = {"23","33","19","45","32","12","23"};
 
     @Override
+    /**
+     * Main activity class
+     *
+     * Aqui muestra los datos 1
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -35,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
+            /**
+             * Main activity class
+             *
+             * Aqui muestra los datos 2
+             */
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                textView.setText("Edad de " + listView.getItemAtPosition(position) + " es de " + edades[position] + " años");
             }
